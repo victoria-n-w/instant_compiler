e64bc8c6cc7d39dcbc59053d0e4cd30fd3049c2f
ajw <mj.wesolowski@student.uw.edu.pl>
2022-11-04 13:00:29 +0100

    automation

--- test.sh
+++ test.sh
@@ -9,25 +9,12 @@ make
 
 compiler=./compiler
 
-
-for code in $(ls examples/*.in); do
+for code in $(ls examples/*.ins); do
     echo ""
     out=$(mktemp)
 
-    llvm_out=$(mktemp)
-    
-    $compiler llvm < $code > $llvm_out 2> /dev/null
-
-    if [[ $? -eq 0 ]]; then
-        echo -e "\e[0;32mllvm $code compiled\e[0m"
-    else
-        echo -e "\e[0;31mllvm $code compilation failed\e[0m"
-        continue
-    fi
-
-    bytecode=$(mktemp)
-
-    llvm-as $llvm_out -o $bytecode
+    bytecode="${code%ins}bc"
+    ./insc_llvm $code
 
     if [[ $? -eq 0 ]]; then
         echo -e "\e[0;32mllvm $code passed by llvm\e[0m"
@@ -40,25 +27,15 @@ for code in $(ls examples/*.in); do
 
     lli $bytecode > $out
 
-    expected_out="${code%in}out"
+    expected_out="${code%ins}out"
     if diff $expected_out $out > /dev/null; then
         echo -e "\e[0;32mllvm $code output ok\e[0m"
     else
         echo -e "\e[0;31mllvm $code - incorrect output\e[0m"
     fi
 
-    jvm_out=/tmp/Name.j
     
-    $compiler jvm < $code > $jvm_out 2> /dev/null
-
-    if [[ $? -eq 0 ]]; then
-        echo -e "\e[0;32mjvm $code compiled\e[0m"
-    else
-        echo -e "\e[0;31mjvm $code compilation failed\e[0m"
-        continue
-    fi
-
-    java -jar lib/jasmin.jar $jvm_out > /dev/null
+    ./insc_jvm $code
 
     if [[ $? -eq 0 ]]; then
         echo -e "\e[0;32mjvm $code passed by jvm\e[0m"
@@ -68,10 +45,14 @@ for code in $(ls examples/*.in); do
         continue
     fi
 
+    filename=$(basename $code)
+    classname=${filename%.ins}
+    pushd $(dirname $code) > /dev/null
+    java $classname > $out
+    popd > /dev/null
 
-    java Name > $out
 
-    expected_out="${code%in}out"
+    expected_out="${code%ins}out"
     if diff $expected_out $out > /dev/null; then
         echo -e "\e[0;32m$code output ok\e[0m"
     else
